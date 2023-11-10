a5f6bd7fe0623af2efd50218af0b0ff90482d8d0
Alexecrable <alexandre.panne@gmail.com>
2023-11-11 00:35:57 +0100

    initialisation de variables pour faciliter les futurs tests et création de la fonction pour calculer la puissance a installer

--- script.js
+++ script.js
@@ -21,11 +21,44 @@ function calc_puissance_jour(surfaceJour, hauteurJour, G10, tempInt, tempExt){//
 function calc_puissance_nuit(surfaceNuit, hauteurNuit, G10, tempInt, tempExt){// W
     return surfaceNuit * hauteurNuit * G10 * (tempInt-tempExt);
 }
+
+
+//=((E13+E14)/G17/G18)*1,2
+function calc_puissance_a_installer(puissanceJour, puissanceNuit, g17, g18){
+    return ((puissanceJour+puissanceNuit) / g17 / g18) * 1.2;
+}
+
+//definition des valeurs de base
+
+surfaceJour = 84;
+hauteurJour = 2.5;
+surfaceNuit = 47;
+hauteurNuit = 2.5;
+
+//valeurs ou je dois encore faire les calculs 
+
+tauxBrassage = 5;
+g8 = 0.8;
+g9 = -5;
+g10 = 20;
+g17 = 0.94;
+g18 = 0.85;
+
 //tests des calculs
 
-console.log("volume : " + calc_volume(84, 2.5, 47, 2.5));
-console.log("debitairzonejour : " + calc_debit_air_jour(84, 2.5, 5));
-console.log("debitairzonenuit : " + calc_debit_air_nuit(47, 2.5, 5));
-console.log("puissance_satisf_jour : " + calc_puissance_jour(84, 2.5, 0.8, 19, -4));
-console.log("puissance_satisf_jour : " + calc_puissance_jour(47, 2.5, 0.8, 19, -4));
+let volume = calc_volume(84, 2.5, 47, 2.5);
+let debitAirZoneJour = calc_debit_air_jour(84, 2.5, 5);
+let debitAirZoneNuit = calc_debit_air_nuit(47, 2.5, 5);
+let puissanceJour = calc_puissance_jour(84, 2.5, 0.8, 19, -4);
+let puissanceNuit = calc_puissance_jour(47, 2.5, 0.8, 19, -4);
+let puissanceInstalle = calc_puissance_a_installer(puissanceJour, puissanceNuit, g17, g18);
+
+
+//affichage des tests
+console.log("volume : " + volume);
+console.log("debitairzonejour : " + debitAirZoneJour);
+console.log("debitairzonenuit : " + debitAirZoneNuit);
+console.log("puissance_satisf_jour : " + puissanceJour);
+console.log("puissance_satisf_nuit : " + puissanceNuit);
+console.log("puissance a installer : " + puissanceInstalle);
 
